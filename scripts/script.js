d55873365aa680e855ba227757a5ab6c798a25cf
hotShot1211 <anshusuperstar15@gmail.com>
2021-07-15 12:05:39 +0530

    ui to be fixed(made prettier)

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,40 @@
 let gridContainer = document.querySelector('.grid-container');
 
 let container = document.querySelector('.container');
-console.log(container);
+let currColor;
 
-function drawGrid(num){
-    for(let i = 0; i < (num*num); i++){
-        let div = document.createElement('div');
-        div.classList.add('cell');
-    
-        container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr)`);
-        // container.setAttribute('style', `grid-template-rows: repeat(${4}, 1fr)`);
-        container.append(div);
+let size = document.querySelector('.size-container');
+
+function drawGrid(num) {
+    if (num < 90) {
+        for (let i = 0; i < (num * num); i++) {
+            let div = document.createElement('div');
+            div.classList.add('cell');
+
+            container.setAttribute('style', `grid-template-columns: repeat(${num}, 1fr)`);
+            container.append(div);
+        }
     }
+    else {
+        num = prompt('please enter a number less than 90');
+        drawGrid(num);
+    }
+    size.innerText = `${num} X ${num}`;
+}
+
+function colorChange(e) {
+    if (e.currentTarget.style.backgroundColor == '') {
+        e.currentTarget.style.backgroundColor = changeColor();
+    }
+}
+
+function changeColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    currColor = `rgb(${r}, ${g}, ${b})`
+
+    return currColor;
 }
 
 let num = prompt("Enter size of grid");
@@ -21,8 +44,21 @@ drawGrid(num);
 let cells = document.querySelectorAll('.cell');
 
 cells.forEach((cell) => {
-    cell.addEventListener('mouseover', (e) => {
-        console.log('hello');
-        e.currentTarget.style.backgroundColor = 'white';
-    })
-})
+    cell.addEventListener('mouseleave', colorChange);
+});
+
+let reset = document.querySelector('#reset');
+
+reset.addEventListener('click', () => {
+    container.innerHTML = '';
+    num = prompt('Enter size of grid');
+    drawGrid(num);
+    cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseleave', colorChange);
+    });
+
+});
+
+
